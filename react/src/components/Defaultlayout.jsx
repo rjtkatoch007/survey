0f12a0570ce61d0a41c8fffc70cd237ba1d8ad1f
rajat <rajat.k@sphinxworldbiz.co>
2024-10-20 13:11:09 +0530

    GuestLayout component created. Login nd signup templates modified.

---
+++ react/src/components/Defaultlayout.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Defaultlayout = () => {
+    return (
+        <div>Defaultlayout</div>
+    )
+}
+
+export default Defaultlayout
--- react/src/router.jsx
+++ react/src/router.jsx
@@ -1,10 +1,32 @@
 import { createBrowserRouter } from "react-router-dom";
-import App from "./App";
+import Dashboard from "./views/Dashboard";
+import Login from "./views/Login";
+import Signup from "./views/Signup";
+import Surveys from "./views/Surveys";
+import GuestLayout from "./components/GuestLayout";
 
 const router = createBrowserRouter([
     {
         path: '/',
-        element: <App />
+        element: <Dashboard />
+    },
+    {
+        path: '/surveys',
+        element: <Surveys />
+    },
+    {
+        path: '/',
+        element: <GuestLayout />,
+        children: [
+            {
+                path: '/login',
+                element: <Login />
+            },
+            {
+                path: '/signup',
+                element: <Signup />
+            },
+        ]
     }
 ])
 

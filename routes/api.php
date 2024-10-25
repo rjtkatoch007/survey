860466e34060b12b30203066a0daa108ca247f44
rajat <rajat.k@sphinxworldbiz.co>
2024-10-25 17:18:13 +0530

    Login and signup requests created with AuthController in Server. APi route installed with axios client created.

---
+++ routes/api.php
@@ -0,0 +1,12 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::get('/signup', [AuthController::class, 'signup']);
+Route::get('/login', [AuthController::class, 'login']);

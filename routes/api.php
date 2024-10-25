1920be6a346c9f20633520b37702e68e61834272
rajat <rajat.k@sphinxworldbiz.co>
2024-10-25 19:14:41 +0530

    Signup page finished with values inserted in DB.

--- routes/api.php
+++ routes/api.php
@@ -8,5 +8,5 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::get('/signup', [AuthController::class, 'signup']);
-Route::get('/login', [AuthController::class, 'login']);
+Route::post('/signup', [AuthController::class, 'signup']);
+Route::post('/login', [AuthController::class, 'login']);
